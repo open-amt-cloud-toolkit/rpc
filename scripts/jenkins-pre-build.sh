1a4da657f10ee25e307ef9718182fe35c0a54488
Mike <michael.johanson@intel.com>
2021-02-09 14:19:58 -0700

    Update jenkins-pre-build.sh

--- scripts/jenkins-pre-build.sh
+++ scripts/jenkins-pre-build.sh
@@ -4,12 +4,12 @@
 #   - Ubuntu 18.04
 #
 
-sudo apt install git cmake build-essential curl zip unzip tar pkg-config
+apt install git cmake build-essential curl zip unzip tar pkg-config
 
 ## current dir - RPC source directory
 export BASE_DIR="$PWD"
-
-cd "$BASE_DIR"/rpc
+ls
+# cd "$BASE_DIR"/rpc
 ## build vcpkg
 git clone --branch 2020.11-1 https://github.com/microsoft/vcpkg.git
 cd vcpkg
