d9225fd2b7608eba420fea783ef68841efd1e167
Mudit Vats <mudit.vats@intel.com>
2021-03-17 11:12:16 -0700

    feature: add heartbeat request / response message support.

---
+++ heartbeat.h
@@ -0,0 +1,13 @@
+/*********************************************************************
+* Copyright (c) Intel Corporation 2019 - 2020
+* SPDX-License-Identifier: Apache-2.0
+**********************************************************************/
+
+#ifndef __HEARTBEAT_H__
+#define __HEARTBEAT_H__
+
+#include <string>
+
+bool heartbeat_create_response(std::string& response);
+
+#endif 
